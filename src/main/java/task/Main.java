7f7b3a604c3ca2c9941ce49f32007e83886c8f44
Michal Wrona <wrona.michal4444@gmail.com>
2023-02-01 10:44:25 +0100

    Aplikacja bankowa 2.1.0 basics

--- src/main/java/task/Main.java
+++ src/main/java/task/Main.java
@@ -1,5 +1,8 @@
 package task;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,7 +13,46 @@ public class Main {
 //        Zestaw1.ramka();
 //        Zestaw1.ramkaPrzekatne();
 //        Zestaw1.piramida();
-        Zestaw1.rocznePrzychody(12);
-
+//        Zestaw1.rocznePrzychody(12);
+        BankAccount bankAccount = new BankAccount(200.0);
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Wpłata na konto "+ "\n" +"2. Wypłata z konta" +"\n"+
+    "3. Menu przycisków" + "\n" + "4. Wyjście z programu");
+        boolean inputIsNotInteger = true;
+        while(inputIsNotInteger) {
+            try {
+                Integer.parseInt(input.nextLine());
+                inputIsNotInteger = false;
+            } catch (NumberFormatException e) {
+                inputIsNotInteger = true;
+                System.out.println("Proszę wpisać liczbę (1,2,3,4)");
+            }
+        }
+        int choice = input.nextInt();
+        boolean switchOn = true;
+        while (switchOn) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Jaką kwotę chcesz wpłacić?");
+                    bankAccount.deposit_funds(input.nextDouble());
+                    break;
+                case 2:
+                    System.out.println("Jaką kwotę chcesz wypłacić?");
+                    bankAccount.withdraw_funds(input.nextDouble());
+                    break;
+                case 3:
+                    System.out.println("1. Wpłata na konto "+ "\n" +"2. Wypłata z konta" +"\n"+
+                            "3. Menu przycisków" + "\n" + "4. Wyjście z programu");
+                    break;
+                case 4:
+                    System.out.println("Dziękujemy za skorzystanie z programu");
+                    switchOn = false;
+                    break;
+                default:
+                    switchOn = false;
+                        break;
+            }
+            choice = input.nextInt();
+        }
     }
 }
